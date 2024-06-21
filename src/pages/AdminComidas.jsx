8e3c8ee8bf985a73bfb53bd7647ab931d3ef85a1
Mariano Vázquez <mareanovazquez@protonmail.com>
2024-06-21 15:40:39 -0300

    pruebas en administración carta comidas

--- src/pages/AdminComidas.jsx
+++ src/pages/AdminComidas.jsx
@@ -23,8 +23,9 @@ export const AdminComidas = () => {
     };
 
     return (
-        <div>
-            <h2>COMIDAS</h2>
+    <>
+        <div className="container-form-admin">
+            <h4>Añadir nuevos productos</h4>
             <form onSubmit={(e) => { e.preventDefault(); addMenuItem(); }}>
                 <label htmlFor="item-name">Nombre del ítem:</label>
                 <input
@@ -56,6 +57,51 @@ export const AdminComidas = () => {
                 <button type="submit">Agregar ítem</button>
             </form>
 
+           
+        </div>
+        <div className="seccion seccion-comidas">
+                <h2>Comidas</h2>
+                <div className="menu-container menu-comidas">
+                    <div className="menu-header">
+                        <span className="header-item"></span>
+                        <span className="header-item"></span>
+                    </div>
+                    <div className="menu-body">
+                        <div className="menu-item">
+                            <p className="item-name">Tostada con palta</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                        <div className="menu-item">
+                            <p className="item-name">Tostado clásico</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                        <div className="menu-item">
+                            <p className="item-name">Sandwich vegano</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                        <div className="menu-item">
+                            <p className="item-name">Pan de queso</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                        <div className="menu-item">
+                            <p className="item-name">Medialunas saladas</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                        <div className="menu-item">
+                            <p className="item-name">Sandwich de pollo</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                        <div className="menu-item">
+                            <p className="item-name">Huevos revueltos</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                        <div className="menu-item">
+                            <p className="item-name">Pizza individual</p>
+                            <p className="item-price">$0000.00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="menu-body">
                 {menuItems.map((item, index) => (
                     <div className="menu-item" key={index}>
@@ -67,6 +113,6 @@ export const AdminComidas = () => {
                     </div>
                 ))}
             </div>
-        </div>
+        </>
     );
 };
